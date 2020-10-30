13de43c2ff3cded5f1e8f59912904973b00bb66f
johno <johnotters@yahoo.com>
2020-10-29 17:35:32 -0700

    Wrote code that replaces the current stage with a new stage from the same group

--- Libraries/Brawl/gmGlobalModeMelee.h
+++ Libraries/Brawl/gmGlobalModeMelee.h
@@ -5,11 +5,18 @@
 #ifndef PROJECTMCODES_GMGLOBALMODEMELEE_H
 #define PROJECTMCODES_GMGLOBALMODEMELEE_H
 
+#include "stddef.h"
 
 struct gmGlobalModeMelee {
-    char _spacer[0x320];
+    char _spacer[0x1B];
+
+    u8 stageID;
+
+    char _spacer2[0x320 - 0x1B - sizeof(stageID)];
 }__attribute__((packed, aligned(2)));
 
+
+static_assert(sizeof(gmGlobalModeMelee) == 0x320, "gmGlobalModeMelee has incorrect size");
 
 #define GM_GLOBAL_MODE_MELEE ((gmGlobalModeMelee*) 0x90180f20)
 
