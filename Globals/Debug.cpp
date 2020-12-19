91c2d97d13addc72f47b6823c32af5ea040a0421
johno <johnotters@yahoo.com>
2020-12-18 19:35:37 -0800

    Improves ASSERTIONs

--- Globals/Debug.cpp
+++ Globals/Debug.cpp
@@ -5,10 +5,9 @@
 #include "Debug.h"
 
 
-void _assert(bool condition) {
-    if(condition == false) {
-        assert();
-    }
+void ASSERTION_ERROR(const char* condition, const char* function, const char* file, int lineNum) {
+    //Put breakpoint on function to catch assertion errors
+    //Use arguments to filter
+    OSReport("Assertion Error in function %s, at %s::%d\n", function, file, lineNum);
+    OSReport("Condition: %s\n", condition);
 }
-
-void assert() {}
